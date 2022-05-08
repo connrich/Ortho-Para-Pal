7e4e0646b8a58f239b561d9fd6deb7a9550ceea6
Connor <connrich3@gmail.com>
2022-05-07 22:01:08 -0700

    Showing data menu in visualization tab

--- Resources/CustomWidgets.py
+++ Resources/CustomWidgets.py
@@ -1,12 +1,16 @@
 import os
 import json
+import typing
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import (QApplication, QMessageBox, QWidget, QGridLayout, QHBoxLayout, QVBoxLayout,
-                                QLabel, QLineEdit, QPushButton, QFileDialog, QTreeView, QCheckBox)
+                                QLabel, QLineEdit, QPushButton, QFileDialog, QTreeView, QColorDialog,
+                                QCheckBox, QListWidget, QListWidgetItem)
 from PyQt5.QtGui import QFont, QIcon
 from pandas.api.types import is_numeric_dtype
 import pandas as pd
 import sys
+import random
+import typing
 
 
 
@@ -204,11 +208,89 @@ class RangeInputWidget(QWidget):
         self.HighInput.setText(str(rnge[1]))
 
 
+class GraphSettingsWidget(QWidget):
+    def __init__(self, GraphDisplay=None, title='Graph Properties'):
+        super().__init__()
+
+        self.Layout = QVBoxLayout()
+        self.Layout.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.setLayout(self.Layout)
+
+        self.title = title
+        self.TitleLabel = QLabel(self.title)
+        self.TitleLabel.setFixedHeight(40)
+        self.TitleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.Layout.addWidget(self.TitleLabel)
+
+        self.DataItems = QVBoxLayout()
+        self.Layout.addLayout(self.DataItems)
+        self.addDataset(None, None, 'file')
+
+        self.GraphDisplay = GraphDisplay
+
+    def addDataset(self, parent_graph, df, name, color=None):
+        item = DataItem(self, parent_graph, df, name, color)
+        self.DataItems.addLayout(item)
+    
+    def removeDataset(self, DataItem):
+        self.DataItems.removeItem(DataItem)
+    
+    
+class DataItem(QHBoxLayout):
+    def __init__(self, parent, parent_graph, data, name, color=None):
+        super().__init__()
+
+        # Parent GraphSettingWidget
+        self.parent = parent
+
+        # Graphwidget where this data is displayed
+        self.parent_graph = parent_graph
+
+        # Set the name of the data
+        self.name = name
+        self.NameLabel = QLabel(name)
+        self.addWidget(self.NameLabel) 
+        
+        # Set the color for display
+        self.ColorButton = QPushButton()
+        self.ColorButton.setFixedWidth(28)
+        self.addWidget(self.ColorButton)
+        if color is None:
+            self.setColor((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        else:
+            self.setColor(color)
+        
+        # Button for deleting the data
+        self.DeleteButton = QPushButton()
+        self.DeleteButton.setText('X')
+        self.DeleteButton.setFixedWidth(28)
+        self.DeleteButton.clicked.connect(self.delete)
+        self.DeleteButton.clicked.connect(self.DeleteButton.deleteLater)
+        self.addWidget(self.DeleteButton)
+
+        self.setContentsMargins(0, 0, 0, 0)
+    
+    def setName(self, name: str) -> None:
+        self.name = name
+        self.NameLabel.setText(self.name)
+    
+    def setColor(self, color: tuple) -> None:
+        r, g, b = color
+        self.ColorButton.setStyleSheet(f"QPushButton {{ background-color: rgb({r}, {g}, {b});}};")  
+
+    def delete(self) -> None:
+        self.parent.removeDataset(self)
+        for i in reversed(range(self.count())):
+            self.itemAt(i).widget().deleteLater()
+        self.deleteLater()
+
+            
+
 if __name__ == '__main__':
     QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
     app = QApplication(sys.argv)
 
-    test_widget = SettingsWindow(settings=None)
+    test_widget = GraphSettingsWidget()
     test_widget.show()
 
     sys.exit(app.exec())
