293b9d7ec52efbfa8e6171dc8de82ccc2de577db
Connor <connor.richards@quantum-technology.com>
2022-05-25 14:51:41 -0700

    Added help option to menu bar

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 import sys
 import json
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QMenuBar, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QMenuBar, QMenu, QAction, QTextEdit
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore, QtWidgets
 from Resources.FileSearchPage import FileSearchPage
@@ -20,6 +20,8 @@ class MainWindow(QMainWindow):
 
         self.loadSettings()
 
+        self.loadHelp()
+
         self.constructMenuBar()
 
         self.constructTabs()
@@ -32,7 +34,19 @@ class MainWindow(QMainWindow):
         # Create settings window
         self.SettingsWindow = SettingsWindow(settings=self.settings)
         self.SettingsWindow.setWindowIcon(QIcon(os.path.join(os.path.dirname(__file__), 'Resources\\SettingsGear.png')))
-    
+
+    def loadHelp(self):
+        with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as help_file:
+            help_text = help_file.read()
+
+        # Create help window
+        self.HelpWindow = QTextEdit()
+        self.HelpWindow.setWindowTitle('Help')
+        self.HelpWindow.setMinimumSize(600, 600)
+        self.HelpWindow.setReadOnly(True)
+        self.HelpWindow.setWindowIcon(QIcon(os.path.join(os.path.dirname(__file__), 'Resources\\HelpQuestionMark.png')))
+        self.HelpWindow.setMarkdown(help_text)
+
     def constructMenuBar(self):
         # Create menu bar
         self.MenuBar = QMenuBar()
@@ -43,6 +57,12 @@ class MainWindow(QMainWindow):
         self.settingsAction.triggered.connect(self.SettingsWindow.showWindow)
         self.MenuBar.addAction(self.settingsAction)
 
+        # Add help menu to menu bar
+        self.helpAction = QAction('Help')
+        self.helpAction.triggered.connect(self.HelpWindow.show)
+        self.helpAction.triggered.connect(self.HelpWindow.activateWindow)
+        self.MenuBar.addAction(self.helpAction)
+
     def constructTabs(self):
         # Initialize tab widget
         self.Tabs = QTabWidget()
@@ -51,7 +71,7 @@ class MainWindow(QMainWindow):
         # Initialize file searching page and functionality
         self.SearchTab = FileSearchPage(self.settings)
         self.SettingsWindow.ApplyButton.clicked.connect(self.SearchTab.settingsUpdated) # Signal for settings update
-        self.Tabs.addTab(self.SearchTab, 'File Search')
+        self.Tabs.addTab(self.SearchTab, 'Ratio Finder')
 
         # Initialize visualization page and functionality
         self.VisualizePage = VisualizePage(self.settings)
